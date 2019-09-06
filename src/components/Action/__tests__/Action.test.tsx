95781bc221c29c5f36c34f2c5dbf343a553ae304
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-06 16:45:39 +0200

    fix(action): refact action and actionAsync component (#57)

--- src/components/Action/__tests__/Action.test.tsx
+++ src/components/Action/__tests__/Action.test.tsx
@@ -1,18 +1,41 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import renderer from 'react-test-renderer'
-import { configureStore, ELifeAction, putCredentials } from 'ts-foursquare'
+import {
+  configureStore,
+  EVenuesAction,
+  getVenuesSearchAsync,
+  ELifeAction,
+  putCredentials,
+} from 'ts-foursquare'
 import { Action } from '..'
 import { PutCredentialsForm } from '../../Forms/Life'
 
 const store = configureStore()
 
 describe('Component/Action', () => {
-  it('renders correctly', () => {
+  it('should make a snapshot after action: ELifeAction.PUT_CREDENTIALS', () => {
     const props = {
-      name: ELifeAction.PUT_CREDENTIALS,
-      actionCaller: putCredentials,
+      action: putCredentials,
       actionPayload: { clientId: '', clientSecret: '' },
+      actionType: ELifeAction.PUT_CREDENTIALS,
+      renderFormPayload: () => <PutCredentialsForm />,
+    }
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Action {...props} />
+        </Provider>
+      )
+      .toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should make a snapshot after action: EVenuesAction.GET_VENUES_SEARCH_REQUES', () => {
+    const props = {
+      action: getVenuesSearchAsync.request,
+      actionPayload: { query: '' },
+      actionType: EVenuesAction.GET_VENUES_SEARCH_REQUEST,
       renderFormPayload: () => <PutCredentialsForm />,
     }
     const tree = renderer
