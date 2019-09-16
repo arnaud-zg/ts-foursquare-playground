9b5462aa32ab2626251b09ac99ec2bd0021d15db
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-09-17 00:05:17 +0200

    fix(decorator): rollback fix on decorator typing

--- src/hoc/withRedux.tsx
+++ src/hoc/withRedux.tsx
@@ -1,9 +1,10 @@
-import React, { ReactNode } from 'react'
+import { RenderFunction } from '@storybook/react'
+import React from 'react'
 import { Provider } from 'react-redux'
 import { configureStore } from 'ts-foursquare'
 
 const store = configureStore()
 
-export const withRedux = (story: () => ReactNode) => (
+export const withRedux = (story: RenderFunction) => (
   <Provider store={store}>{story()}</Provider>
 )
