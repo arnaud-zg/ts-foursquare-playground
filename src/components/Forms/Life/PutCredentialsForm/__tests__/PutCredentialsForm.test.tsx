42247d7b3c36c251a938b875434a3c7d67fd8142
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2019-12-20 16:43:55 +0100

    fix: add middlewares parameter on configure store

--- src/components/Forms/Life/PutCredentialsForm/__tests__/PutCredentialsForm.test.tsx
+++ src/components/Forms/Life/PutCredentialsForm/__tests__/PutCredentialsForm.test.tsx
@@ -4,7 +4,7 @@ import renderer from 'react-test-renderer'
 import { configureStore } from 'ts-foursquare'
 import { PutCredentialsForm } from '..'
 
-const store = configureStore()
+const store = configureStore({ middlewares: [] })
 
 describe('Component/PutCredentialsForm', () => {
   it('renders correctly', () => {
