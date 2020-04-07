b24c0e2ae1497d54fde18a670ea22bb5857bf144
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-04-08 00:32:47 +0200

    chore: use rebase on release script

--- script/release.sh
+++ script/release.sh
@@ -21,14 +21,14 @@ ask_confirm_and_release () {
               yarn
               git checkout release
               git pull origin release
-              git merge develop
+              git rebase develop
               yarn run release
               git checkout master
               git pull origin master
-              git merge release
+              git rebase release
               git checkout develop
               git pull origin develop
-              git merge release
+              git rebase release
               git push origin release
               git push origin develop
               git push origin --follow-tags master
